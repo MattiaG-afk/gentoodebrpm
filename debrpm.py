a331532928117d1caf865b6857ee9add0a1a33b6
gasmat <53443116+MattiaG-afk@users.noreply.github.com>
2021-04-13 10:42:14 +0200

    Update debrpm.py

--- debrpm.py
+++ debrpm.py
@@ -10,8 +10,6 @@ if not len(sys.argv[1:]) or sys.argv[1] == '-h' or sys.argv[1] == '--help':
     print("OPTIONS:")
     print("-h, --help\t\tShow this help message")
     print("-i, --install\t\tInstall a packet from a file .deb or .rpm")
-    print("-s, --search\t\tSearch for a packet")
-    print("-u, --update\t\tUpdate the local list of available packets in the repo")
 else:
     if sys.argv[1] == '-i' or sys.argv[1] == '--install':
         file = sys.argv[2]
@@ -40,27 +38,8 @@ else:
             subprocess.run(command, shell=True)
         else:
             print("Unknown file. Currently supported files are: .deb and .rpm")
-    if sys.argv[1] == '-s' or sys.argv[1] == '--search':
-        find = 0
-        print("Search results:")
-        file = open('/var/db/debrpm/packages.txt', 'r')
-        packets = file.readlines()
-        for packet in packets:
-            if packet.find(sys.argv[2]) != -1:
-                find = 1
-                packet_name = packet[0:packet.find('[')]
-                packet_description = packet[(packet.find('[')+1):packet.find(']')]
-                packet_url = packet[(packet.find('(')+1):packet.find(')')]
-                print("\033[1;31mPacket\033[0m:", packet_name)
-                print("\033[;32mDescription\033[0m:", packet_description)
-                print("\033[;32mURL\033[0m:", packet_url)
-                if packet_url.find('.deb') != -1:
-                    print("\033[;32mType\033[0m: .deb\n")
-                elif packet_url.find('.rpm') != -1:
-                    print("\033[;32mType\033[0m: .rpm\n")
-        if not find:
-            print("The packet is not in the repository")
-    if sys.argv[1] == '-u' or sys.argv[1] == '--update':
-        print("Updating the /var/db/debrpm/packages.txt file...")
-        r = requests.get('https://raw.githubusercontent.com/MattiaG-afk/debrpm-repo/main/packages.txt', allow_redirects=True)
-        open('/var/db/debrpm/packages.txt', 'wb').write(r.content)
+    else:
+        print("\033[1;31mUsage\033[0m: debrpm [OPTIONS] [FILE]\n")
+        print("OPTIONS:")
+        print("-h, --help\t\tShow this help message")
+        print("-i, --install\t\tInstall a packet from a file .deb or .rpm")
