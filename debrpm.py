26f92d3d9b286ae6f5bb1606591554a0a9dba2bc
gasmat <53443116+MattiaG-afk@users.noreply.github.com>
2021-04-21 15:59:00 +0200

    Update debrpm.py

--- debrpm.py
+++ debrpm.py
@@ -2,44 +2,48 @@
 import subprocess
 import sys
 import os
-import getopt
-import requests
 
-if not len(sys.argv[1:]) or sys.argv[1] == '-h' or sys.argv[1] == '--help':
+options = {}
+i = 0
+for option in sys.argv:
+    if option.startswith('-'):
+        options[option] = sys.argv[i+1]
+    i += 1
+
+if '-i' in options or '--install' in options:
+    try:
+        file = options['-i']
+    except:
+        file = options['--install']
+    if file.find('.deb') != -1:
+        print("Installing the file: ", file)
+        command = "sudo ar x " + file
+        subprocess.run(command, shell=True)
+        subprocess.run("sudo rm debian-binary control.tar.xz", shell=True)
+        subprocess.run("sudo mv data.tar.xz /", shell=True)
+        os.chdir("/")
+        command = "sudo tar xpvf data.tar.xz >> /var/log/debrpm/" + file + ".log"
+        subprocess.run(command, shell=True)
+        subprocess.run("rm /data.tar.xz", shell=True)
+    elif file.find('.rpm') != -1:
+        print("Installing the file: ", file)
+        command = "rpm2tarxz " + file
+        subprocess.run(command, shell=True)
+        command = "rm " + file
+        subprocess.run(command, shell=True)
+        command = "mv " + file.replace(".rpm", ".tar.xz") + " /"
+        subprocess.run(command, shell=True)
+        os.chdir("/")
+        command = "sudo tar xpvf /" + file.replace(".rpm", ".tar.xz") + " >> /var/log/debrpm/" + file + ".log"
+        subprocess.run(command, shell=True)
+        command= "rm /" + file.replace(".rpm", ".tar.xz")
+        subprocess.run(command, shell=True)
+    else:
+        print('\u001b[31;1mUnknown file. Currently supported files are: .deb and .rpm\u001b[00;0m')
+elif '-u' in options or '--uninstall' in options:
+    print('Coming soon')
+else:
     print("\033[1;31mUsage\033[0m: debrpm [OPTIONS] [FILE]\n")
     print("OPTIONS:")
     print("-h, --help\t\tShow this help message")
     print("-i, --install\t\tInstall a packet from a file .deb or .rpm")
-else:
-    if sys.argv[1] == '-i' or sys.argv[1] == '--install':
-        file = sys.argv[2]
-        if file.find('.deb') != -1:
-            print("Installing the file: ", file)
-            command = "sudo ar x " + file
-            subprocess.run(command, shell=True)
-            subprocess.run("sudo rm debian-binary control.tar.xz", shell=True)
-            subprocess.run("sudo mv data.tar.xz /", shell=True)
-            os.chdir("/")
-            command = "sudo tar xpvf data.tar.xz >> /var/log/debrpm/" + file + ".log"
-            subprocess.run(command, shell=True)
-            subprocess.run("rm /data.tar.xz", shell=True)
-        elif file.find('.rpm') != -1:
-            print("Installing the file: ", file)
-            command = "rpm2tarxz " + file
-            subprocess.run(command, shell=True)
-            command = "rm " + file
-            subprocess.run(command, shell=True)
-            command = "mv " + file.replace(".rpm", ".tar.xz") + " /"
-            subprocess.run(command, shell=True)
-            os.chdir("/")
-            command = "sudo tar xpvf /" + file.replace(".rpm", ".tar.xz") + " >> /var/log/debrpm/" + file + ".log"
-            subprocess.run(command, shell=True)
-            command= "rm /" + file.replace(".rpm", ".tar.xz")
-            subprocess.run(command, shell=True)
-        else:
-            print("Unknown file. Currently supported files are: .deb and .rpm")
-    else:
-        print("\033[1;31mUsage\033[0m: debrpm [OPTIONS] [FILE]\n")
-        print("OPTIONS:")
-        print("-h, --help\t\tShow this help message")
-        print("-i, --install\t\tInstall a packet from a file .deb or .rpm")
