1a5cbbfda86385ea3030659ebf47b4bbf49f253c
gasmat <53443116+MattiaG-afk@users.noreply.github.com>
2021-04-28 21:39:51 +0200

    Update debrpm.py

--- debrpm.py
+++ debrpm.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import os. sys, subprocess
+import os, sys, subprocess
 
 options = {}
 i = 0
@@ -20,7 +20,8 @@ if '-i' in options or '--install' in options:
             root = options['--root']
     else:
         root = '/'
-    logfile = file + '.log'
+    logfile = '/var/log/debrpm/' + file + '.log'
+    print(logfile)
     open(logfile, 'w').write('Root directory:' + root)
     if file.find('.deb') != -1:
         print("Installing the file: ", file)
@@ -29,7 +30,7 @@ if '-i' in options or '--install' in options:
         subprocess.run("sudo rm debian-binary control.tar.xz", shell=True)
         subprocess.run("sudo mv data.tar.xz %s" % root, shell=True)
         os.chdir(root)
-        command = "sudo tar xpvf data.tar.xz >> /var/log/debrpm/" + logfile
+        command = "sudo tar xpvf data.tar.xz >> " + logfile
         subprocess.run(command, shell=True)
         subprocess.run("rm data.tar.xz", shell=True)
     elif file.find('.rpm') != -1:
@@ -41,17 +42,38 @@ if '-i' in options or '--install' in options:
         command = "mv " + file.replace(".rpm", ".tar.xz") + " " + root
         subprocess.run(command, shell=True)
         os.chdir(root)
-        command = "sudo tar xpvf " + file.replace(".rpm", ".tar.xz") + " >> /var/log/debrpm/" + logfile
+        command = "sudo tar xpvf " + file.replace(".rpm", ".tar.xz") + " >> " + logfile
         subprocess.run(command, shell=True)
         command= "rm " + file.replace(".rpm", ".tar.xz")
         subprocess.run(command, shell=True)
     else:
         print('\u001b[31;1mUnknown file. Currently supported files are: .deb and .rpm\u001b[00;0m')
 elif '-u' in options or '--uninstall' in options:
-    pass
+    try:
+        packet = options['-u']
+    except:
+        packet = options['--uninstall']
+    if not packet.startswith('/var/log/debrpm/'):
+        packet = '/var/log/debrpm/' + packet
+    if not packet.endswith('.log'):
+        packet += '.log'
+    try:
+        packet_log = open(packet)
+        root = packet_log.readline().split('Root directory:')[1].replace('./','')
+        if root != '/':
+            print('Deleting files: ')
+            for line in packet_log.readlines():
+                print(line.replace('./', root + '/').replace('\n', ''), end='\n')
+                subprocess.run('rm -rf ' + line.replace('./', root + '/').replace('\n', ''), shell=True)
+
+    except:
+        print('Package not installed')
 
 else:
     print("\033[1;31mUsage\033[0m: debrpm [OPTIONS] [FILE]\n")
     print("OPTIONS:")
     print("-h, --help\t\tShow this help message")
     print("-i, --install\t\tInstall a packet from a file .deb or .rpm")
+    print("-u, --uninstall\t\tUninstall a packet from a log file of a .deb or .rpm installed packet\n")
+    print('\033[1;00mINSTALL OPTIONS\033[0m:')
+    print('-/, --root\t\tChoose the directory where to extract the package')
