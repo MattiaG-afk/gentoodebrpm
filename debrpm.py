965f6ec95f389ed840abf1da9853f015d9265b8f
gasmat <53443116+MattiaG-afk@users.noreply.github.com>
2021-05-06 16:40:23 +0200

    Update debrpm.py

--- debrpm.py
+++ debrpm.py
@@ -5,7 +5,10 @@ options = {}
 i = 0
 for option in sys.argv:
     if option.startswith('-'):
-        options[option] = sys.argv[i+1]
+        if not('help' in option or 'h' in option) and not('list' in option or 'l' in option):
+            options[option] = sys.argv[i+1]
+        else:
+            options[option] = ''
     i += 1
 
 if '-i' in options or '--install' in options:
@@ -20,7 +23,7 @@ if '-i' in options or '--install' in options:
             root = options['--root']
     else:
         root = '/'
-    logfile = '/var/log/debrpm/' + file + '.log'
+    logfile = os.path.join('/var/log/debrpm/', file + '.log')
     open(logfile, 'w').write('Root directory:' + root)
     if file.find('.deb') != -1:
         print("Installing the file: ", file)
@@ -53,7 +56,7 @@ elif '-u' in options or '--uninstall' in options:
     except:
         packet = options['--uninstall']
     if not packet.startswith('/var/log/debrpm/'):
-        packet = '/var/log/debrpm/' + packet
+        packet = os.path.join('/var/log/debrpm', packet)
     if not packet.endswith('.log'):
         packet += '.log'
     try:
@@ -73,12 +76,23 @@ elif '-u' in options or '--uninstall' in options:
                 subprocess.run('rmdir ' + directory, shell=True)
     except:
         print('Package not installed')
+elif '-l' in options or '--list' in options:
+    index = 0
+    for file in os.listdir('/var/log/debrpm'):
+        if file.endswith('.log'):
+            index += 1
+            if '.deb' in file:
+                print('\u001b[33;1mName\u001b[00;0m: %s, \u001b[33;1mtype\u001b[00;0m: deb, \u001b[33;1mlog file\u001b[00;0m: %s' % (file.replace('.deb.log', ''), os.path.join('/var/log/debrpm', file)))
+            elif '.rpm' in file:
+                print('\u001b[33;1mName\u001b[00;0m: %s, \u001b[33;1mtype\u001b[00;0m: rpm, \u001b[33;1mlog file\u001b[00;0m: %s' % (file.replace('.rpm.log', ''), os.path.join('/var/log/debrpm', file)))
+    print('\u001b[36;1mNumber of installed packages\u001b[00;0m: %s' % index)
 
 else:
     print("\033[1;31mUsage\033[0m: debrpm [OPTIONS] [FILE]\n")
     print("OPTIONS:")
     print("-h, --help\t\tShow this help message")
     print("-i, --install\t\tInstall a packet from a file .deb or .rpm")
-    print("-u, --uninstall\t\tUninstall a packet from a log file of a .deb or .rpm installed packet\n")
+    print("-u, --uninstall\t\tUninstall a packet from a log file of a .deb or .rpm installed packet")
+    print("-l, --list\t\tList all installed packages\n")
     print('\033[1;00mINSTALL OPTIONS\033[0m:')
     print('-/, --root\t\tChoose the directory where to extract the package')
