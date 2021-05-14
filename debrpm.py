1bbf254928956928e25d84925d079a59d9ff749e
gasmat <53443116+MattiaG-afk@users.noreply.github.com>
2021-05-14 17:52:05 +0200

    Update debrpm.py

--- debrpm.py
+++ debrpm.py
@@ -30,12 +30,12 @@ if '-i' in options or '--install' in options:
     open(log_file, 'w').write('Root directory:' + root)
     if file.find('.deb') != -1:
         print("Installing the file: ", file)
-        subprocess.run("sudo ar x " + file, shell=True)
-        subprocess.run("rm -f debian-binary control.tar.xz", shell=True)
-        subprocess.run("sudo mv data.tar.xz %s" % root, shell=True)
-        os.chdir(root)
-        subprocess.run("sudo tar xpvf data.tar.xz >> " + log_file, shell=True)
-        subprocess.run("rm -f data.tar.xz", shell=True)
+        subprocess.run("sudo cp %s %s" % (file, tmp_dir), shell=True)
+        subprocess.run("sudo ar x %s" % os.path.join(tmp_dir, file), shell=True)
+        subprocess.run("rm -f %s %s" % (os.path.join(tmp_dir, "debian-binary"), os.path.join(tmp_dir, "control.tar.xz")), shell=True)
+        subprocess.run("sudo tar xpvf %s >> %s" % (os.path.join(tmp_dir, "data.tar.xz"), os.path.join(log_dir, log_file)), shell=True)
+        subprocess.run("rm -f %s" % os.path.join(tmp_dir, "data.tar.xz"), shell=True)
+        subprocess.run("mv %s %s" % (os.path.join(tmp_dir, "*"), root), shell=True)
     elif file.find('.rpm') != -1:
         print("Installing the file: ", file)
         subprocess.run("rpm2tarxz " + file, shell=True)
