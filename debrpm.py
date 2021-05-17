10990b07c53ac33a884c12a690f6f21b773f366d
gasmat <53443116+MattiaG-afk@users.noreply.github.com>
2021-05-17 19:15:35 +0200

    Update debrpm.py

--- debrpm.py
+++ debrpm.py
@@ -3,8 +3,9 @@ import os, sys, subprocess
 
 options = {}
 log_dir = '/var/log/debrpm'
-i = 0
+tmp_dir = '/var/tmp/debrpm'
 
+i = 0
 for option in sys.argv:
     if option.startswith('-'):
         if not('help' in option or 'h' in option) and not('list' in option or 'l' in option):
@@ -28,21 +29,22 @@ if '-i' in options or '--install' in options:
     log_file = os.path.join(log_dir, file + '.log')
     open(log_file, 'w').write('Root directory:' + root)
     if file.find('.deb') != -1:
-        print("Installing the file: ", file)
-        subprocess.run("sudo ar x " + file, shell=True)
-        subprocess.run("rm -f debian-binary control.tar.xz", shell=True)
-        subprocess.run("sudo mv data.tar.xz %s" % root, shell=True)
-        os.chdir(root)
-        subprocess.run("sudo tar xpvf data.tar.xz >> " + log_file, shell=True)
-        subprocess.run("rm -f data.tar.xz", shell=True)
+        print("Installing the file:", file)
+        subprocess.run("sudo cp %s %s" % (os.path.join(os.curdir, file), os.path.join(tmp_dir, file)), shell=True)
+        os.chdir(tmp_dir)
+        subprocess.run("sudo ar x %s" % os.path.join(tmp_dir, file), shell=True)
+        subprocess.run("rm -f %s %s" % (os.path.join(tmp_dir, "debian-binary"), os.path.join(tmp_dir, "control.tar.xz")), shell=True)
+        subprocess.run("sudo tar xpvf %s >> %s" % (os.path.join(tmp_dir, "data.tar.xz"), log_file), shell=True)
+        subprocess.run("rm -f %s" % os.path.join(tmp_dir, "data.tar.xz"), shell=True)
+        subprocess.run("mv %s %s" % (os.path.join(tmp_dir, "*"), root), shell=True)
     elif file.find('.rpm') != -1:
-        print("Installing the file: ", file)
-        subprocess.run("rpm2tarxz " + file, shell=True)
-        subprocess.run("rm -f %s" % file, shell=True)
-        subprocess.run("mv " + file.replace(".rpm", ".tar.xz") + " " + root, shell=True)
-        os.chdir(root)
-        subprocess.run("sudo tar xpvf " + file.replace(".rpm", ".tar.xz") + " >> " + log_file, shell=True)
-        subprocess.run("rm " + file.replace(".rpm", ".tar.xz"), shell=True)
+        print("Installing the file:", file)
+        subprocess.run("sudo cp %s %s" % (os.path.join(os.curdir, file), os.path.join(tmp_dir, file)), shell=True)
+        os.chdir(tmp_dir)
+        subprocess.run("rpm2tarxz %s" % os.path.join(os.curdir, file), shell=True)
+        subprocess.run("sudo tar xpvf %s >> %s" % (os.path.join(os.curdir, file.replace(".rpm", ".tar.xz")), log_file), shell=True)
+        subprocess.run("rm -f %s %s" %(os.path.join(os.curdir, file), os.path.join(os.curdir, file.replace(".rpm", ".tar.xz"))), shell=True)
+        subprocess.run("mv %s %s" % (os.path.join(tmp_dir, "*"), root), shell=True)
     else:
         print('\u001b[31;1mUnknown file. Currently supported files are: .deb and .rpm\u001b[00;0m')
 elif '-u' in options or '--uninstall' in options:
@@ -50,8 +52,7 @@ elif '-u' in options or '--uninstall' in options:
         packet = options['-u']
     except:
         packet = options['--uninstall']
-    if not packet.startswith(log_dir):
-        packet = os.path.join(log_dir, packet)
+    packet = os.path.join(log_dir, packet)
     if not packet.endswith('.log'):
         packet += '.log'
     try:
@@ -71,16 +72,13 @@ elif '-u' in options or '--uninstall' in options:
                 subprocess.run('rmdir ' + directory, shell=True)
             subprocess.run("rm -f %s" % os.path.join(log_dir, packet), shell=True)
     except:
-        print('Package not installed')
+        print('Exited with error:', sys.exc_info[1])
 elif '-l' in options or '--list' in options:
     index = 0
     for file in os.listdir(log_dir):
         if file.endswith('.log'):
             index += 1
-            if '.deb' in file:
-                print('\u001b[33;1mName\u001b[00;0m: %s, \u001b[33;1mtype\u001b[00;0m: deb, \u001b[33;1mlog file\u001b[00;0m: %s' % (file.replace('.deb.log', ''), os.path.join(log_dir, file)))
-            elif '.rpm' in file:
-                print('\u001b[33;1mName\u001b[00;0m: %s, \u001b[33;1mtype\u001b[00;0m: rpm, \u001b[33;1mlog file\u001b[00;0m: %s' % (file.replace('.rpm.log', ''), os.path.join(log_dir, file)))
+            print('\u001b[33;1mName\u001b[00;0m: %s, \u001b[33;1mtype\u001b[00;0m: %s, \u001b[33;1mlog file\u001b[00;0m: %s' % (file.replace('.deb.log', ''), "deb" if ".deb" in file else "rpm", os.path.join(log_dir, file)))
     print('\u001b[36;1mNumber of installed packages\u001b[00;0m: %s' % index)
 
 else:
