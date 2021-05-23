376e7b291896fe7c61864e78bc7000277362a7d8
gasmat <53443116+MattiaG-afk@users.noreply.github.com>
2021-05-23 22:35:11 +0200

    Update debrpm-gui.py

--- debrpm-gui.py
+++ debrpm-gui.py
@@ -1,7 +1,105 @@
+import os, sys, subprocess
+from tkinter.filedialog import askopenfilename
 from tkinter import *
+
+options = {}
+log_dir = '/var/log/debrpm'
+tmp_dir = '/var/tmp/debrpm'
+
+def installFile(file):
+    print(file)
+    print('Root dir:', root_dir.get())
+
+def uninstallFile(file):
+    print(file)
+    print('Root dir:', root_dir.get())
+
+'''
+i = 0
+for option in sys.argv:
+    if option.startswith('-'):
+        if not('help' in option or 'h' in option) and not('list' in option or 'l' in option):
+            options[option] = sys.argv[i+1]
+        else:
+            options[option] = ''
+    i += 1
+
+if '-i' in options or '--install' in options:
+    try:
+        file = options['-i']
+    except:
+        file = options['--install']
+    if '-/' in options or '--root' in options:
+        try:
+            root = options['-/']
+        except:
+            root = options['--root']
+    else:
+        root = '/'
+    log_file = os.path.join(log_dir, file + '.log')
+    open(log_file, 'w').write('Root directory:' + root)
+    if file.find('.deb') != -1:
+        print("Installing the file:", file)
+        subprocess.run("sudo cp %s %s" % (os.path.join(os.curdir, file), os.path.join(tmp_dir, file)), shell=True)
+        os.chdir(tmp_dir)
+        subprocess.run("sudo ar x %s" % os.path.join(tmp_dir, file), shell=True)
+        subprocess.run("rm -f %s %s" % (os.path.join(tmp_dir, "debian-binary"), os.path.join(tmp_dir, "control.tar.xz")), shell=True)
+        subprocess.run("sudo tar xpvf %s >> %s" % (os.path.join(tmp_dir, "data.tar.xz"), log_file), shell=True)
+        subprocess.run("rm -f %s" % os.path.join(tmp_dir, "data.tar.xz"), shell=True)
+        subprocess.run("mv %s %s" % (os.path.join(tmp_dir, "*"), root), shell=True)
+    elif file.find('.rpm') != -1:
+        print("Installing the file:", file)
+        subprocess.run("sudo cp %s %s" % (os.path.join(os.curdir, file), os.path.join(tmp_dir, file)), shell=True)
+        os.chdir(tmp_dir)
+        subprocess.run("rpm2tarxz %s" % os.path.join(os.curdir, file), shell=True)
+        subprocess.run("sudo tar xpvf %s >> %s" % (os.path.join(os.curdir, file.replace(".rpm", ".tar.xz")), log_file), shell=True)
+        subprocess.run("rm -f %s %s" %(os.path.join(os.curdir, file), os.path.join(os.curdir, file.replace(".rpm", ".tar.xz"))), shell=True)
+        subprocess.run("mv %s %s" % (os.path.join(tmp_dir, "*"), root), shell=True)
+    else:
+        print('\u001b[31;1mUnknown file. Currently supported files are: .deb and .rpm\u001b[00;0m')
+elif '-u' in options or '--uninstall' in options:
+    try:
+        packet = options['-u']
+    except:
+        packet = options['--uninstall']
+    packet = os.path.join(log_dir, packet)
+    if not packet.endswith('.log'):
+        packet += '.log'
+    try:
+        packet_log = open(packet)
+        root = packet_log.readline().split('Root directory:')[1].replace('./','')
+        dir = []
+        if root != '/':
+            for line in packet_log.readlines():
+                remove = root.replace('\n', '') + line.replace('\n', '').replace('./', '/')
+                if os.path.isdir(remove):
+                    dir.append(remove)
+                else:
+                    subprocess.run('rm -f ' + line.replace('\n', ''), shell=True)
+            dir.sort()
+            dir.reverse()
+            for directory in dir:
+                subprocess.run('rmdir ' + directory, shell=True)
+            subprocess.run("rm -f %s" % os.path.join(log_dir, packet), shell=True)
+    except:
+        print('Exited with error:', sys.exc_info[1])
+elif '-l' in options or '--list' in options:
+    index = 0
+    for file in os.listdir(log_dir):
+        if file.endswith('.log'):
+            index += 1
+            print('\u001b[33;1mName\u001b[00;0m: %s, \u001b[33;1mtype\u001b[00;0m: %s, \u001b[33;1mlog file\u001b[00;0m: %s' % (file.replace('.deb.log', ''), "deb" if ".deb" in file else "rpm", os.path.join(log_dir, file)))
+    print('\u001b[36;1mNumber of installed packages\u001b[00;0m: %s' % index)
+'''
+
 
 root = Tk()
 root.title('Debrpm GUI')
-Label(root, text='Skeleton for debrpm GUI').pack(expand=YES, fill=BOTH)
+Label(root, text='Select the file that you want to install:').pack(side=TOP, expand=YES, fill=BOTH)
+root_dir = Entry(root)
+root_dir.insert(0, '/')
+root_dir.pack(side=TOP, expand=YES, fill=X)
+Button(root, text='Install', command=(lambda: installFile(askopenfilename()))).pack(side=RIGHT, fill=BOTH)
+Button(root, text='Uninstall', command=(lambda: uninstallFile(askopenfilename()))).pack(side=RIGHT, fill=BOTH)
 
 root.mainloop()
